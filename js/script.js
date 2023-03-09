c1d1add6a81cdb86c032f83a1bf785c9927de318
Juan David Holguin <juandaho@gmail.com>
2023-03-09 15:19:48 -0500

    realizando carrito

--- js/script.js
+++ js/script.js
@@ -48,31 +48,3 @@ btn_registro.addEventListener("click", usuario_registrado);
 let btn_login = document.getElementById("btn_login");
 btn_login.addEventListener("click", login_usuario);
 
-// // Script para carrito de compras
-
-// const btnCart = document.querySelector(".container-cart-icon");
-// const containterCartProducts = document.querySelector(".container-cart-products");
-
-// btnCart.addEventListener("click", function(){
-
-//     containterCartProducts.classList.toggle("hidden-cart")
-
-// })
-
-// /* ==================  */
-
-// const cartInfo =document.querySelector(".cart-product");
-// const rowProduct = document.querySelector(".row-product");
-
-// //Lista de todos los contenedores de producto
-// const productsList = document.querySelector(".container-items");
-
-// //Variable de arreglo de Productos
-
-// let allProducts = [];
-
-// productsList.addEventListener("click", function (e){
-
-//     console.log(e.target);
-
-// })
---
+++ js/scritp_carrito.js
@@ -0,0 +1,77 @@
+const btnCart = document.querySelector(".container-cart-icon");
+const containerCartProducts = document.querySelector(
+  ".container-cart-products"
+);
+
+btnCart.addEventListener("click", () => {
+  containerCartProducts.classList.toggle("hidden-cart");
+});
+
+//--------------------------
+
+const cartInfo = document.querySelector(".cart-product");
+const rowProduct = document.querySelector(".row-product");
+
+//Lista de todos los productos
+
+const productList = document.querySelector(".container-items");
+
+//Variable de arreglos
+
+let allProducts = [];
+
+productList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-add-cart")) {
+    const product = e.target.parentElement;
+    const infoProduct = {
+      quantity: 1,
+      title: product.querySelector("h2").textContent,
+      price: product.querySelector("p").textContent,
+    };
+
+    allProducts = [...allProducts, infoProduct];
+    showHTML();
+    
+  }
+});
+
+//Función para mostrar en el HTML
+
+const showHTML = () => {
+  allProducts.forEach((product) => {
+
+//Limpiar HTML
+
+rowProduct.innerHTML="";
+
+    const containerProduct = document.createElement("div");
+    containerProduct.classList.add("cart-product");
+
+    containerProduct.innerHTML = `
+        
+        <div class="info-cart-product">
+        <span class="cantidad-producto-carrito">${product.quantity}</span>
+        <p class="titulo-producto-carrito">${product.title}</p>
+        <span class="precio-producto-carrito">${product.price}</span>
+      </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="icon-close"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+
+        `;
+
+    rowProduct.append(containerProduct)
+
+  });
+};
